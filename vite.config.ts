cb9c21797f2add68cc963873e3b5152104902850
Illya Soloviov <illyalesnik@gmail.com>
2025-10-18 13:41:12 +0300

    final edits #4

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
         tailwindcss(),
         react(),
     ],
+    base: '/<React-project>/',
     resolve: {
         alias: {
             '@': path.resolve(__dirname, 'src'),
